51ff590337d9a66a665f0e91bdc86c128065ffc4
Clay Gibson <blackcreekproductions@gmail.com>
2023-03-28 16:24:16 -0400

    updated landing page

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -4,32 +4,75 @@ import classes from './LandingPage.module.css';
 import NewSeller from './NewSellerPage.js';
 import AuthContext from '../storage/auth-context';
 import { useContext } from 'react';
-import { FaCarrot } from 'react-icons/fa';
+import {
+	FaCarrot,
+	FaMapMarkerAlt,
+	FaTruck,
+} from 'react-icons/fa';
+
+import { IoLogoVenmo } from 'react-icons/io5';
 
 const LandingPage = (props) => {
 	const authCtx = useContext(AuthContext);
 
 	return (
-		<main className="container">
-			<section>
-				<h1 className={classes.title}>NAME OF APP HERE</h1>
-				<FaCarrot className={classes.carrot} />
-
-				<h2 className={classes.heading}>
-					Find Food That Is Grown Close To Home
+		<main
+			className={`container ${classes.landingPageContainer}`}
+		>
+			<section className={classes.heroSection}>
+				<h2 className={classes.headingText}>
+					Find your next meal from a local farm or backyard
+					gardner
 				</h2>
-				<h4 className={classes.subHeading}>
-					Meet local producers near you. From the gardener
-					down the street to full time farmers.
-				</h4>
+
 				<NavLink to="/all-sellers">
 					<button>View Producers</button>
 				</NavLink>
 			</section>
-			<section></section>
+			<ul className={classes.howItWorks}>
+				<li>
+					<h3 className={classes.title}>How it works</h3>
+				</li>
+				<li>
+					<FaMapMarkerAlt className={classes.icon} />
+					<h4 className={classes.subHeading}>
+						Look on the map local producers near you. From
+						the gardener down the street to full time
+						farmers.
+					</h4>
+					<NavLink to="/all-sellers">
+						<button>View Producers</button>
+					</NavLink>
+				</li>
+				<li>
+					<FaCarrot className={classes.icon} />
+					<h4 className={classes.subHeading}>
+						Browse the food products they offer
+					</h4>
+
+					<NavLink to="/profile">
+						<ButtonElement buttonText="Sign Up" />
+					</NavLink>
+				</li>
+				<li>
+					<FaTruck className={classes.icon} />
+					<h4 className={classes.subHeading}>
+						Quickly see where and when you can pick it up
+					</h4>
+				</li>
+				<li>
+					<IoLogoVenmo className={classes.icon} />
+					<h4 className={classes.subHeading}>
+						Pay easily with Venmo
+					</h4>
+				</li>
+			</ul>
 
 			<NavLink to="/new-seller">
-				<ButtonElement buttonText="Sign up to be a seller" />
+				<ButtonElement buttonText="Become a seller" />
+			</NavLink>
+			<NavLink to="/all-sellers">
+				<ButtonElement buttonText="View On Map" />
 			</NavLink>
 		</main>
 	);
